05c3565b24db6d2fa5dfcc319783c95feff00dcd
joao0antonio <147611658+joao0antonio@users.noreply.github.com>
2025-03-17 10:59:56 -0300

    a

--- java.js
+++ java.js
@@ -1,18 +1,20 @@
-let tamanhoFonte = 16;
-const texto = document.getElementById("texto");
-const botaoAumentar = document.getElementById("aumentar");
-const botaoDiminuir = document.getElementById("diminuir");
+document.addEventListener("DOMContentLoaded", function () {
+    let tamanhoFonte = 16;
+    const texto = document.getElementById("texto");
+    const botaoAumentar = document.getElementById("aumentar");
+    const botaoDiminuir = document.getElementById("diminuir");
 
-botaoAumentar.addEventListener("click", () => {
-    if (tamanhoFonte < 30) {
-        tamanhoFonte += 2;
-        texto.style.fontSize = tamanhoFonte + "px";
-    }
-});
+    botaoAumentar.addEventListener("click", () => {
+        if (tamanhoFonte < 30) {
+            tamanhoFonte += 2;
+            texto.style.fontSize = tamanhoFonte + "px";
+        }
+    });
 
-botaoDiminuir.addEventListener("click", () => {
-    if (tamanhoFonte > 10) {
-        tamanhoFonte -= 2;
-        texto.style.fontSize = tamanhoFonte + "px";
-    }
+    botaoDiminuir.addEventListener("click", () => {
+        if (tamanhoFonte > 10) {
+            tamanhoFonte -= 2;
+            texto.style.fontSize = tamanhoFonte + "px";
+        }
+    });
 });
